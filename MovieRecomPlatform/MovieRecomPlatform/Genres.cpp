d1a4a0edc6913c0e02af8e6225d631a20df1611a
Cosmin <TBP57@yahoo.com>
2022-11-12 23:12:43 +0200

    Added constructor, copy constructor, destructor, getters and setters in Genres.cpp.

--- MovieRecomPlatform/MovieRecomPlatform/Genres.cpp
+++ MovieRecomPlatform/MovieRecomPlatform/Genres.cpp
@@ -1,1 +1,27 @@
 #include "Genres.h"
+
+Genres::Genres(int genreId, std::string name) :
+	m_genreId{ genreId },
+	m_name{ name }
+{
+}
+
+int Genres::GetGenreId() const
+{
+	return m_genreId;
+}
+
+void Genres::SetGenreId(int genreId)
+{
+	m_genreId = genreId;
+}
+
+std::string Genres::GetName() const
+{
+	return m_name;
+}
+
+void Genres::SetName(std::string name)
+{
+	m_name = name;
+}
