98c873c7c8da11d7c150107e80663f98b399c1b2
alexmicato <alex.micato@yahoo.com>
2022-11-19 17:50:24 +0200

    Added birthdate and gender to user along with declaration of Setters & Getters

--- MovieRecomPlatform/MovieRecomPlatform/User.h
+++ MovieRecomPlatform/MovieRecomPlatform/User.h
@@ -6,6 +6,8 @@ private:
 	int m_userId;
 	std::string m_username;
 	std::string m_password;
+	std::string birthdate;
+	std::string gender;
 
 public:
 	User(int userId, std::string username, std::string password);
@@ -14,9 +16,13 @@ public:
 	int GetUserId() const;
 	std::string GetUsername() const;
 	std::string GetPassword() const;
+	std::string GetPassword() const;
+	std::string GetGender() const;
 	void SetUserId(int userId);
-	void SetUsername(std::string userName);
-	void SetPassword(std::string passWord);
+	void SetUsername(std::string username);
+	void SetPassword(std::string password);
+	void SetBirthdate(std::string birthdate);
+	void SetGender(std::string gender);
 
 };
 
