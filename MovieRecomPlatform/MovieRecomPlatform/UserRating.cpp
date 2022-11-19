7adfbdf6300c730458f33aa81cee25dda9600fd0
alexmicato <alex.micato@yahoo.com>
2022-11-19 18:03:07 +0200

    Updates Users database and created UserRating class

---
+++ MovieRecomPlatform/MovieRecomPlatform/UserRating.cpp
@@ -0,0 +1,1 @@
+#include "UserRating.h"
---
+++ MovieRecomPlatform/MovieRecomPlatform/UserRating.h
@@ -0,0 +1,24 @@
+#pragma once
+#include <memory>
+class UserRating
+{
+private:
+	std::unique_ptr <int> m_userId;
+	std::unique_ptr <int> m_movieId;
+	int m_rating;
+
+public:
+	UserRating(std::unique_ptr <int> userId, std::unique_ptr <int> movieId, int rating);
+	UserRating(const UserRating& userRating) = default;
+	~UserRating() = default;
+	std::unique_ptr <int> GetUserId() const;
+	std::unique_ptr <int> GetMovieId() const;
+	int GetRating() const;
+	void SetUserId(std::unique_ptr <int> userId);
+	void SetMovieId(std::unique_ptr <int> movieId);
+	void SetRating(int rating);
+
+
+
+};
+
