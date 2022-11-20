6517d414d471be29683d36f70481d3949386177d
Cosmin <TBP57@yahoo.com>
2022-11-20 13:16:04 +0200

    Created the LikedGenre class, added constructor, destructor, getters and setters in the header.

---
+++ MovieRecomPlatform/MovieRecomPlatform/LikedGenre.cpp
@@ -0,0 +1,22 @@
+#include "LikedGenre.h"
+
+LikedGenre::LikedGenre(std::unique_ptr<int> userId, std::unique_ptr<int> genreId)
+{}
+
+const std::unique_ptr<int>& LikedGenre::GetUserID() const
+{
+	// TODO: insert return statement here
+}
+
+const std::unique_ptr<int>& LikedGenre::GetGenreID() const
+{
+	// TODO: insert return statement here
+}
+
+void LikedGenre::SetUserID(std::unique_ptr<int> userID)
+{
+}
+
+void LikedGenre::SetGenreID(std::unique_ptr<int> genreID)
+{
+}
---
+++ MovieRecomPlatform/MovieRecomPlatform/LikedGenre.h
@@ -0,0 +1,18 @@
+#pragma once
+#include <memory>
+class LikedGenre
+{
+private:
+	std::unique_ptr<int> m_userID;
+	std::unique_ptr<int> m_genreID;
+
+public:
+	LikedGenre(std::unique_ptr<int> userId, std::unique_ptr<int> genreId);
+	~LikedGenre() = default;
+
+	const std::unique_ptr<int>& GetUserID() const;
+	const std::unique_ptr<int>& GetGenreID() const;
+	void SetUserID(std::unique_ptr<int> userID);
+	void SetGenreID(std::unique_ptr<int> genreID);
+};
+
