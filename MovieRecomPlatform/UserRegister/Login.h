e0971082a24a6d3df60fa2889520d433e8b9aef8
alexmicato <alex.micato@yahoo.com>
2022-11-26 16:48:53 +0200

    Added getters/setters definition to Login class

---
+++ MovieRecomPlatform/UserRegister/Login.h
@@ -0,0 +1,35 @@
+#pragma once
+#include <string>
+
+#ifdef LOGIN_EXPORTS
+#define LOGIN_API __declspec(dllexport)
+#else 
+#define LOGIN_API __declspec(dllimport)
+#endif // LOGGING_EXPORTS
+
+class LOGIN_API Login
+{
+
+	enum class AuthentificationStatus
+	{
+		Successful,
+		NotRegistered,
+		WrongPassword,
+		Error
+	};
+
+private:
+	std::string m_username;
+	std::string m_password;
+
+public:
+	Login(const std::string& username, const std::string& password);
+
+	void SetUsername(const std::string& username);
+	std::string GetUsername() const;
+	void SetPassword(const std::string& password);
+	std::string GetPassword() const;
+
+};
+
+
