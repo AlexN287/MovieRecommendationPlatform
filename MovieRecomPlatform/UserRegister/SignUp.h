bf028dcbcb0d75a75f5dc1b257f02e5366be34a1
Cosmin <TBP57@yahoo.com>
2022-11-26 16:25:35 +0200

    Added SignUp class to Register DLL

--- MovieRecomPlatform/UserRegister/SignUp.h
+++ MovieRecomPlatform/UserRegister/SignUp.h
@@ -1,6 +1,12 @@
 #pragma once
 
-class SignUp
+#ifdef SIGNUP_EXPORTS
+#define SIGNUP_API __declspec(dllexport)
+#else 
+#define SIGNUP_API __declspec(dllimport)
+#endif // SIGNUP_EXPORTS
+
+class SIGNUP_API SignUp
 {
 };
  
