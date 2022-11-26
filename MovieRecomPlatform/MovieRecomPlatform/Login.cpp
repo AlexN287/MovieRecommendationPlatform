ab5c8c07e77d492b14625064fb8f40bf974edf08
alexmicato <alex.micato@yahoo.com>
2022-11-26 22:38:15 +0200

    Added definition to setters/getters of Login class

--- MovieRecomPlatform/MovieRecomPlatform/Login.cpp
+++ MovieRecomPlatform/MovieRecomPlatform/Login.cpp
@@ -1,1 +1,24 @@
 #include "Login.h"
+
+Login::Login(const std::string& username, const std::string& password) : m_username{ username }, m_password{ password }
+{}
+
+void Login::SetUsername(const std::string& username)
+{
+	this->m_username = username;
+}
+
+std::string Login::GetUsername() const
+{
+	return m_username;
+}
+
+void Login::SetPassword(const std::string& password)
+{
+	this->m_password = password;
+}
+
+std::string Login::GetPassword() const
+{
+	return m_password;
+}
