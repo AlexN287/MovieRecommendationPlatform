b2c1487e37f8c95021b4f1a834080df9f7e66000
alexmicato <alex.micato@yahoo.com>
2022-11-26 22:37:12 +0200

    Added definition to Login class

--- MovieRecomPlatform/MovieRecomPlatform/Login.h
+++ MovieRecomPlatform/MovieRecomPlatform/Login.h
@@ -1,5 +1,26 @@
 #pragma once
+#include <string>
 class Login
 {
+	enum class AuthentificationStatus
+	{
+		Successful,
+		NotRegistered,
+		WrongPassword,
+		Error
+	};
+
+private:
+	std::string m_username;
+	std::string m_password;
+
+public:
+	Login(const std::string& username, const std::string& password);
+
+	void SetUsername(const std::string& username);
+	std::string GetUsername() const;
+	void SetPassword(const std::string& password);
+	std::string GetPassword() const;
+
 };
 
