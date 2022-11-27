ecddbb71e22b48c997d82e54417c8eb3f51e382e
Cosmin <TBP57@yahoo.com>
2022-11-27 17:27:32 +0200

    Added constructor, getters and setters in LikedActor.cpp

--- MovieRecomPlatform/MovieRecomPlatform/LikedActors.cpp
+++ MovieRecomPlatform/MovieRecomPlatform/LikedActors.cpp
@@ -1,13 +1,24 @@
 #include "LikedActors.h"
 
-LikedActors::LikedActors(std::unique_ptr<int> userId, std::unique_ptr<int> actorId)
+LikedActors::LikedActors(std::unique_ptr<int> userId, std::unique_ptr<int> actorId) :
+	m_userId{ std::move(userId) }, m_actorId{ std::move(actorId) }{}
+
+const std::unique_ptr<int>& LikedActors::GetActorId() const
 {
+	return m_actorId;
+}
+
+const std::unique_ptr<int>& LikedActors::GetUserId() const
+{
+	return m_userId;
 }
 
 void LikedActors::SetActorId(std::unique_ptr<int> actorId)
 {
+	m_actorId = std::move(actorId);
 }
 
 void LikedActors::SetUserId(std::unique_ptr<int> userId)
 {
+	m_userId = std::move(userId);
 }
