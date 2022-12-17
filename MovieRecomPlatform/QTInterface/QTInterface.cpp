6eb401eee5b14ff5346b18679ff195be4ad91b1e
EneClaudiu <eneclaudiu02@gmail.com>
2022-12-17 16:09:30 +0200

    Moved the login groupBox in a stacked widget.

--- MovieRecomPlatform/QTInterface/QTInterface.cpp
+++ MovieRecomPlatform/QTInterface/QTInterface.cpp
@@ -1,7 +1,7 @@
 #include "QTInterface.h"
 #include "QMessageBox"
 
-QTInterface::QTInterface(QWidget *parent)
+QTInterface::QTInterface(QWidget* parent)
     : QMainWindow(parent)
 {
     ui.setupUi(this);
@@ -12,11 +12,11 @@ QTInterface::~QTInterface()
 
 void QTInterface::on_pushButton_login_clicked()
 {
-
     std::string username = ui.lineEdit_username->text().toStdString();
     std::string password = ui.lineEdit_password->text().toStdString();
-    if (username == "Test" && password == "Test") {
-        QMessageBox::information(this, "Login", "da");
+    if (username == "test" && password == "test") {
+        //QMessageBox::information(this, "Login", "da");
+        ui.stackedWidget->setCurrentIndex(1);
     }
     else
     {
